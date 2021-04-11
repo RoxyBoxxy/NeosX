c5758e8ee60ae78da842d1a98a4e69f6e9abf1c4
Roxy Squires <squires1993@gmail.com>
2021-04-11 23:07:41 +0100

    Delete countdown.js

--- countdown.js
+++
@@ -1,9 +0,0 @@
-module.exports = function countdown(tick){
-    let count = 10;
-    let timer = setInterval(_ =>{
-      tick(count--);
-      if(count === -1){
-          clearInterval(timer);
-      }
-    }, 1000);
-  }
